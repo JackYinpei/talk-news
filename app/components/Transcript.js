ce10ef8b89ee6dad969098fae71bf7554cbca0b9
root <no@thanks.com>
2025-08-23 14:09:50 +0800

    fix transcript overflow

--- app/components/Transcript.js
+++ app/components/Transcript.js
@@ -86,7 +86,7 @@ function Transcript({
 
   return (
     <div className="flex flex-col flex-1 bg-white min-h-0 rounded-xl">
-      <div className="flex flex-col flex-1 min-h-0">
+      <div className="flex flex-col flex-1 overflow-hidden">
         <div className="flex items-center justify-between px-6 py-3 sticky top-0 z-10 text-base border-b bg-white rounded-t-xl">
           <span className="font-semibold">Transcript</span>
           <div className="flex gap-x-2">
@@ -108,10 +108,11 @@ function Transcript({
         </div>
 
         {/* Transcript Content */}
-        <div
+        <div 
           ref={transcriptRef}
-          className="overflow-auto p-4 flex flex-col gap-y-4 h-full"
+          className="flex-1 overflow-y-auto p-4"
         >
+          <div className="flex flex-col gap-y-4">
           {[...transcriptItems]
             .sort((a, b) => a.createdAtMs - b.createdAtMs)
             .map((item) => {
@@ -221,6 +222,7 @@ function Transcript({
               );
             }
           })}
+          </div>
         </div>
       </div>
 
