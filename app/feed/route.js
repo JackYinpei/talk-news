1db58874ec8b76153bf6176d2fa45cb6c6c76ef4
ChengYang mac <2680887830@qq.com>
2026-01-05 14:47:09 +0800

    fix contact email address error

--- app/feed/route.js
+++ app/feed/route.js
@@ -61,7 +61,7 @@ export async function GET() {
     </itunes:category>
     <itunes:owner>
       <itunes:name>LingDaily</itunes:name>
-      <itunes:email>sliver@linux.do</itunes:email>
+      <itunes:email>slivery@linux.do</itunes:email>
     </itunes:owner>
     <itunes:image href="${imageUrl}" />
     <itunes:explicit>clean</itunes:explicit>
